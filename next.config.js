9665b8fddf54d8ca57cc548e0996acabe7157286
GaryYongYao <doppelganger_tianjay@hotmail.com>
2021-04-22 03:18:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,13 +11,13 @@ module.exports = withImages({
       source: '/api/:path*',
       destination: 'https://desksit.chativo.io/api/:path*' // Proxy to Backend
     }
-  ],
+  ], */
   exportPathMap: function (defaultPathMap) {
     return {
       '/': { page: '/' },
-      '/': { page: '/' },
+      'products/6767': { page: 'products/[code]', query: { code: '6767' } },
     }
-  }, */
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
