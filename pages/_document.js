58da289f1630e69fe52b747a9db9156d8b9a32a5
GaryYongYao <doppelganger_tianjay@hotmail.com>
2021-05-09 05:26:37 +0800

    animation flash fix

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script> </script>
         </body>
       </Html>
     )
