8cbe5320d09637239b02765855b6228403560f63
GaryYongYao <doppelganger_tianjay@hotmail.com>
2021-05-23 04:04:57 +0800

    shopping cart UI

---
+++ pages/cart.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { useContext } from 'react'
+import { Footer, Header } from 'components'
+import { CartContext } from 'utils/cartCookie'
+import { List, NoItem } from 'sections/Cart'
+import styles from 'styles/modules/Cart.module.scss'
+
+function Projects() {
+  const { cartAmount } = useContext(CartContext)
+
+  return (
+    <div className={styles['container']}>
+      <Head>
+        <title>Shopping Cart - Gallaria</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <section className={styles['section-title']}>
+        <div className="container">
+          <div className={styles['title']}>
+            CART {cartAmount > 0 && `(${cartAmount})`}
+          </div>
+        </div>
+      </section>
+
+      {cartAmount < 1 && <NoItem />}
+      {cartAmount > 0 && <List />}
+      <Footer />
+    </div>
+  )
+}
+
+export default Projects
---
+++ sections/Cart/List.js
@@ -0,0 +1,171 @@
+import { useContext, useState } from 'react'
+import { sumBy } from 'lodash'
+import { useRouter } from 'next/router'
+import Cookies from 'js-cookie'
+import { CartContext } from 'utils/cartCookie'
+import Link from 'components/Link'
+import styles from 'styles/modules/Cart.module.scss'
+
+function List() {
+  const [submit, setSubmit] = useState(false)
+  const router = useRouter()
+  const { setCartAmount, shoppingCart, setShoppingCart } = useContext(CartContext)
+
+  const removeItem = (e, i) => {
+    e.preventDefault()
+    const cart = JSON.parse(Cookies.get('cart'))
+    cart.splice(i, 1)
+    Cookies.set('cart', cart)
+
+    setShoppingCart(JSON.parse(Cookies.get('cart')))
+    setCartAmount(JSON.parse(Cookies.get('cart')).length)
+  }
+
+  const addQuantity = (i) => {
+    const cart = JSON.parse(Cookies.get('cart'))
+    cart[i].quantity += 1
+    Cookies.set('cart', cart)
+
+    setShoppingCart(JSON.parse(Cookies.get('cart')))
+    setCartAmount(JSON.parse(Cookies.get('cart')).length)
+  }
+
+  const removeQuantity = (i) => {
+    const cart = JSON.parse(Cookies.get('cart'))
+    if (cart[i].quantity - 1 > 0) cart[i].quantity -= 1
+    Cookies.set('cart', cart)
+
+    setShoppingCart(JSON.parse(Cookies.get('cart')))
+    setCartAmount(JSON.parse(Cookies.get('cart')).length)
+  }
+
+  const handleChange = (e, i) => {
+    const cart = JSON.parse(Cookies.get('cart'))
+    cart[i].quantity = parseInt(e.target.value, 10)
+    if (!e.target.value) cart[i].quantity = 1
+    Cookies.set('cart', cart)
+
+    setShoppingCart(JSON.parse(Cookies.get('cart')))
+    setCartAmount(JSON.parse(Cookies.get('cart')).length)
+  }
+
+  return (
+    <section className={styles['section-list']}>
+      <div className="container">
+        <div className={`${styles['list-title']} row`}>
+          <div className="col-6">
+            PRODUCTS
+          </div>
+          <div className={`${styles['hide-mobile']} ${styles['title-label']} col-lg-2`}>
+            PRODUCTS
+          </div>
+          <div className={`${styles['title-label']} col-6 col-lg-2`}>
+            QTY.
+          </div>
+          <div className={`${styles['hide-mobile']} col-lg-2`}>
+            QTY.
+          </div>
+        </div>
+        <div className={`${styles['divider']} ${styles['divider-1']}`} />
+        <div className={styles['item-container']}>
+          {shoppingCart.map((item, i) => (
+            <div key={item.code} className={`row ${styles['item']}`}>
+              <div className={`${styles['image-container']} col-4 col-lg-3`}>
+                <Link href={`/product/${item.code}`}>
+                  <img src={`${process.env.NEXT_PUBLIC_STORAGE_URL}${encodeURIComponent(item.image).replace('(', '%28').replace(')', '%29')}`} />
+                </Link>
+              </div>
+              <div className={`${styles['text-container']} col-8 col-lg-3`}>
+                <div className={styles['title']}>
+                  <Link href={`/product/${item.code}`}>
+                    {item.name}
+                  </Link>
+                  <span className={styles['mobile-quantity']}>{item.quantity}</span>
+                </div>
+                <div className={`${styles['info']} col-12 col-lg-4`}>
+                  VARIANT: {item.variant || 'N/A'}
+                  <br />
+                  <span className={styles['mobile-price']}>
+                    AUD: ${item.price.toFixed(2)}
+                    <br />
+                  </span>
+                </div>
+                <div className={styles['remove']}>
+                  <a href="#" onClick={(e) => removeItem(e, i)}>
+                    REMOVE
+                  </a>
+                  <a href="#" onClick={(e) => removeItem(e, i)}>
+                    <img src="/svg/bin.svg" alt="remove" />
+                  </a>
+                </div>
+              </div>
+              <div className={`${styles['text-container']} ${styles['hide-mobile']} col-lg-2`}>
+                <div className={`${styles['info']}`} style={{ textAlign: 'center' }}>
+                  AUD: ${item.price.toFixed(2)}
+                </div>
+              </div>
+              <div className={`${styles['text-container']} ${styles['hide-mobile']} col-lg-2`}>
+                <div className={`${styles['info']}`}>
+                  <span className={styles['quantity']}>
+                    <span
+                      onClick={() => removeQuantity(i)}
+                      className={styles['minus']}
+                    >
+                      -
+                    </span>
+                    <input
+                      value={item.quantity}
+                      onChange={e => handleChange(e, i)}
+                      type="number"
+                    />
+                    <span
+                      onClick={() => addQuantity(i)}
+                      className={styles['add']}
+                    >
+                      +
+                    </span>
+                  </span>
+                </div>
+              </div>
+              <div className={`${styles['text-container']} ${styles['hide-mobile']} col-lg-2`}>
+                <div className={`${styles['info']}`} style={{ textAlign: 'right' }}>
+                  AUD: ${(item.price * item.quantity).toFixed(2)}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className={`${styles['divider']} ${styles['divider-2']}`} />
+        <div className={`row ${styles['total-container']}`}>
+          <div className="col-5 col-lg-8" />
+          <div className={`col-1 ${styles['label']}`}>
+            SUBTOTAL:
+          </div>
+          <div className={`col-6 col-lg-3 ${styles['value']}`}>
+            AUD ${sumBy(shoppingCart, item => item.price * item.quantity).toFixed(2)}
+          </div>
+        </div>
+        <div className={`${styles['divider']} ${styles['divider-3']}`} />
+        <div className={`${styles['button-container']} row`}>
+          <div className="col-lg-3">
+            <button
+              type="button"
+              disabled={submit}
+              onClick={() => router.back()}
+              className="button-outlined"
+            >
+              BACK TO BROWSING
+            </button>
+          </div>
+          <div className="col-lg-3" style={{ marginLeft: 'auto' }}>
+            <button type="button" disabled={submit} className="button-contained">
+              ENQUIRE NOW
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default List
