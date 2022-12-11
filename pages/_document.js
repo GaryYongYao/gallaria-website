2f303a8a87b1163616acd6d60e1c496aa943f4bf
GaryYongYao <doppelganger_tianjay@hotmail.com>
2022-12-11 23:20:38 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
           <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossOrigin="anonymous" async />
+          <meta name="google-site-verification" content="dPnq4d_AZYCj4kHxgeua5xtUVBNzKwd0pfp1vq2eBGI" />
           <script src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_CAPTCHA_KEY}`} />
           <link rel="stylesheet" type="text/css" href="https://unpkg.com/fullpage.js@3.0.1/dist/fullpage.min.css" />
           {/*  <!-- Google Tag Manager --> */}
